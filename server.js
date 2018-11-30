3f035fb570a5fbebb6de9d0362efea6d55874a06
pmanlukas <lukas.pollmann@tum.de>
2018-11-30 16:26:36 +0100

    small change

--- server.js
+++ server.js
@@ -155,5 +155,5 @@ app.delete('/api/books/:id', (req, res) => {
 
 // listen on the port 3000
 app.listen(process.env.PORT || 3000, () => {
-  console.log('Express server is up and running on http://localhost:3000/');
+  console.log('My Express server is up and running on http://localhost:3000/');
 });
